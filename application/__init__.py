842aac9658725f89acf1f3edc5fe444b7cf89c12
rishabh557 <singhrishabh557@gmail.com>
2020-06-13 16:51:22 +0530

    HomePage

--- application/__init__.py
+++ application/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 from application import routes
+
+app.static_folder = 'static'
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,15 @@
 from application import app
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for
 
-
+@app.route('/home')
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route('/home/executivelogin')
+def executivelogin():
+    return render_template('executivelogin.html')
+
+@app.route('/home/cashierlogin')
+def cashierlogin():
+    return render_template('cashierlogin.html')
