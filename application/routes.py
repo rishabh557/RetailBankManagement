d7d6744733d186a31b6b937066062c4cb8f20b98
Siddharth Bhasin <siddharth88bhasin@gmail.com>
2020-06-17 13:06:29 +0530

    fixed Routes

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from application import app
 from flask import render_template
 from .models import Cashier, Executive, Accounts, db, Transaction, Customer, login_manager, login_required, login_user, SQLAlchemy
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, flash, render_template, redirect, url_for, request
 
 
 @app.route('/home')
@@ -57,11 +57,11 @@ def load_cashier(username):
     return Cashier.query.get(username)
 
 
-@app.route('/createcustomer', methods = ['GET', 'POST'])
+@app.route('/createcustomer', methods =['GET', 'POST'])
 def createcustomer():
     if request.method == 'POST':
         if not request.form['ssn'] or not request.form['cust_id'] or not request.form['name'] or not request.form['address'] or not request.form['age']:
-            flash('Please enter all the fields' , 'error')
+            flash('Please enter all the fields', 'error')
         else:
             customer = Customer(request.form['ssn'], request.form['cust_id'], request.form['name'], request.form['address'], request.form['age'])
             db.session.add(customer)
@@ -71,14 +71,15 @@ def createcustomer():
 
 
 
-@app.route('/createaccount', methods = ['GET', 'POST'])
-def createcustomer():
+@app.route('/createaccount', methods=['GET', 'POST'])
+def createaccount():
     if request.method == 'POST':
         if not request.form['acc_number'] or not request.form['acc_type'] or not request.form['balance'] or not request.form['cust_id']:
-            flash('Please enter all the fields' , 'error')
+            flash('Please enter all the fields', 'error')
         else:
             account = Accounts(request.form['acc_number'], request.form['acc_type'], request.form['balance'], request.form['cust_id'])
             db.session.add(account)
             db.session.commit()
             flash('Record was successfully added')
             return render_template('executivehome.html')
+
