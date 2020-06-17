fa88736dd4364637f088a079097a099ab510c00f
Siddharth Bhasin <siddharth88bhasin@gmail.com>
2020-06-17 12:49:00 +0530

    routes update

--- application/routes.py
+++ application/routes.py
@@ -55,3 +55,30 @@ def load_executive(username):
 @login_manager.user_loader
 def load_cashier(username):
     return Cashier.query.get(username)
+
+
+@app.route('/createcustomer', methods = ['GET', 'POST'])
+def createcustomer():
+    if request.method == 'POST':
+        if not request.form['ssn'] or not request.form['cust_id'] or not request.form['name'] or not request.form['address'] or not request.form['age']:
+            flash('Please enter all the fields' , 'error')
+        else:
+            customer = Customer(request.form['ssn'], request.form['cust_id'], request.form['name'], request.form['address'], request.form['age'])
+            db.session.add(customer)
+            db.session.commit()
+            flash('Record was successfully added')
+            return render_template('executivehome.html')
+
+
+
+@app.route('/createaccount', methods = ['GET', 'POST'])
+def createcustomer():
+    if request.method == 'POST':
+        if not request.form['acc_number'] or not request.form['acc_type'] or not request.form['balance'] or not request.form['cust_id']:
+            flash('Please enter all the fields' , 'error')
+        else:
+            account = Accounts(request.form['acc_number'], request.form['acc_type'], request.form['balance'], request.form['cust_id'])
+            db.session.add(customer)
+            db.session.commit()
+            flash('Record was successfully added')
+            return render_template('executivehome.html')
