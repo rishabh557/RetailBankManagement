4962c09ad50da15fed60d425ab1352a9acfd6c19
rishabh557 <singhrishabh557@gmail.com>
2020-06-17 10:12:05 +0530

    secret key update

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 file_path = os.path.abspath(os.getcwd()) + "/test1.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
