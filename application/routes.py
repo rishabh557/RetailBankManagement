fa7497dafb5af3ca3b8806f0839f2e296c3c7029
rishabh557 <singhrishabh557@gmail.com>
2020-06-17 10:20:43 +0530

    Logout functionality added

--- application/routes.py
+++ application/routes.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, redirect, url_for, request
 
 
 @app.route('/home')
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
